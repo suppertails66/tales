e66d0d8e6d78155e1901cb12ab6fc914f6810c5a
suppertails66 <suppertails66@gmail.com>
2016-01-22 11:54:55 -0600

    Start filling in TileMap class

--- libtales/include/gamedata/TileMap.h
+++ libtales/include/gamedata/TileMap.h
@@ -3,27 +3,210 @@
 
 
 #include "structs/Tbyte.h"
+#include "gamegear/TileReference.h"
 #include <string>
 
 namespace Tales {
 
 
 /**
- * A rectangular area composed of tiles identifiers.
+ * A rectangular area composed of tile identifiers.
  */
 class TileMap {
 public:
+  /**
+   * Enum of tilemap types.
+   * 2-byte-per-tile format is a standard "full" tilemap with 16-bit
+   * identifiers.
+   * 1-byte-per-tile format specifies only the low byte of each identifier,
+   * with some particular high byte for all values specified in the code.
+   * The main difference is that 1-byte-per-tile format restricts tiles
+   * to a specific range (0-255 or 256-511) and prevents them from being
+   * mirrored, having palette flags set, etc.
+   */
+  enum TileMapFormat {
+	oneBytePerTile,
+	twoBytesPerTile
+  };
+
+  /**
+   * Default constructor.
+   */
   TileMap();
-  TileMap(const Tbyte* data, int w__, int h__);
   
-  int readFromData(const Tbyte* data, int w__, int h__);
+  /**
+   * Constructor from raw data.
+   * @param data Raw data source.
+   * @param format__ Format of the source data.
+   * @param w__ Width in tiles of the source data.
+   * @param h__ Height in tiles of the source data.
+   */
+  TileMap(const Tbyte* data,
+          TileMapFormat format__,
+          int w__,
+		  int h__);
+  
+  /**
+   * Constructor from raw 1bpt data.
+   * @param data Raw data source.
+   * @param format__ Format of the source data.
+   * @param w__ Width in tiles of the source data.
+   * @param h__ Height in tiles of the source data.
+   * @param upperByte__ Upper byte of each tile entry.
+   */
+  TileMap(const Tbyte* data,
+          TileMapFormat format__,
+          int w__,
+		  int h__,
+		  Tbyte upperByte__);
+  
+  /**
+   * Destructor.
+   */
+  ~TileMap();
+  
+  /**
+   * Reads a tilemap from raw data.
+   * @param data Raw data source.
+   * @param format__ Format of the source data.
+   * @param w__ Width in tiles of the source data.
+   * @param h__ Height in tiles of the source data.
+   */
+  int readFromData(const Tbyte* data,
+                   TileMapFormat format__,
+				   int w__,
+				   int h__);
+  
+  /**
+   * Reads a 1bpt tilemap from raw data.
+   * @param data Raw data source.
+   * @param format__ Format of the source data.
+   * @param w__ Width in tiles of the source data.
+   * @param h__ Height in tiles of the source data.
+   */
+  int readFromData(const Tbyte* data,
+                   TileMapFormat format__,
+				   int w__,
+				   int h__,
+				   Tbyte upperByte__);
+  
+  /**
+   * Writes the tilemap to raw data.
+   * @param data Raw data destination.
+   */
   void writeToData(Tbyte* data) const;
   
+  /**
+   * Saves to a string.
+   * @param data String to save to.
+   */
   void save(std::string& data) const;
+  
+  /**
+   * Loads from a byte array.
+   * @param data Byte array to load from.
+   */
   int load(const Tbyte* data);
+  
+  /**
+   * Getter.
+   */
+  TileMapFormat format() const;
+  
+  /**
+   * Getter.
+   */
+  int w() const;
+  
+  /**
+   * Getter.
+   */
+  int h() const;
+  
+  /**
+   * Getter.
+   */
+  int lowerLimit() const;
+  
+  /**
+   * Getter.
+   */
+  int upperLimit() const;
+  
+  /**
+   * Setter.
+   */
+  void setFormat(TileMapFormat format__);
+  
+  /**
+   * Setter.
+   */
+  void setW(int w__);
+  
+  /**
+   * Setter.
+   */
+  void setH(int h__);
+  
+  /**
+   * Setter.
+   */
+  void setLowerLimit(int lowerLimit__);
+  
+  /**
+   * Setter.
+   */
+  void setUpperLimit(int upperLimit__);
 protected:
+  /**
+   * Default lower limit of tilemap range (0 = all tiles).
+   */
+  const static int defaultLowerLimit_ = 0;
+  
+  /**
+   * Default upper limit of tilemap range (512 = all tiles).
+   */
+  const static int defaultUpperLimit_ = 512;
+  
+  /**
+   * Deallocate the tile array.
+   */
+  void destroyTileData();
+  
+  /**
+   * Initialize the tile array to the specified size.
+   */
+  void reinitializeTileData(int w__, int h__);
+  
+  /**
+   * Array of tile data.
+   */
+  TileReference** tileData_;
+  
+  /**
+   * Format of the source data.
+   */
+  TileMapFormat format_;
+  
+  /**
+   * Width in tiles of the tilemap.
+   */
   int w_;
+  
+  /**
+   * Height in tiles of the tilemap.
+   */
   int h_;
+  
+  /**
+   * Lower limit of the tiles that can be specified in the tilemap.
+   */
+  int lowerLimit_;
+  
+  /**
+   * Upper limit of the tiles that can be specified in the tilemap.
+   */
+  int upperLimit_;
 };
 
 
--- libtales/src/gamedata/TileMap.cpp
+++ libtales/src/gamedata/TileMap.cpp
@@ -1,19 +1,116 @@
 #include "gamedata/TileMap.h"
+#include "util/ByteConversion.h"
+#include "util/ByteSizes.h"
+#include <cstdlib>
+
+using namespace Luncheon;
 
 namespace Tales {
 
 
 TileMap::TileMap()
-  : w_(0), h_(0) { };
+  : tileData_(NULL),
+	format_(TileMap::twoBytesPerTile),
+	w_(0),
+	h_(0),
+	lowerLimit_(defaultLowerLimit_),
+	upperLimit_(defaultUpperLimit_) { };
 
-TileMap::TileMap(const Tbyte* data, int w__, int h__)
-  : w_(w__),
-    h_(h__) {
-  readFromData(data, w__, h__);
+TileMap::TileMap(const Tbyte* data,
+	  TileMapFormat format__,
+	  int w__,
+	  int h__)
+  : tileData_(NULL),
+	format_(format__),
+	w_(w__),
+	h_(h__),
+	lowerLimit_(defaultLowerLimit_),
+	upperLimit_(defaultUpperLimit_) {
+  readFromData(data, format_, w_, h_);
 }
 
-int TileMap::readFromData(const Tbyte* data, int w__, int h__) {
+TileMap::TileMap(const Tbyte* data,
+	  TileMapFormat format__,
+	  int w__,
+	  int h__,
+	  Tbyte upperByte__)
+  : format_(format__),
+    w_(w__),
+	h_(h__),
+    lowerLimit_(defaultLowerLimit_),
+    upperLimit_(defaultUpperLimit_) {
+  readFromData(data, format_, w_, h_, upperByte__);
+}
+
+TileMap::~TileMap() {
+  destroyTileData();
+}
+
+int TileMap::readFromData(const Tbyte* data,
+			   TileMapFormat format__,
+			   int w__,
+			   int h__) {
   int byteCount = 0;
+  
+  // Set format data
+  format_ = format__;
+  w_ = w__;
+  h_ = h__;
+  
+  // Reinitialize tile array
+  reinitializeTileData(w_, h_);
+  
+  // Read tile identifiers
+  for (int j = 0; j < h_; j++) {
+    for (int i = 0; i < w_; i++) {
+	  tileData_[i][j] = TileReference(ByteConversion::fromBytes(
+	                              data + byteCount,
+								  ByteSizes::uint16Size,
+								  EndiannessTypes::little,
+								  SignednessTypes::nosign));
+      
+	  // Place in array
+	  byteCount += ByteSizes::uint16Size;
+	}
+  }
+  
+  return byteCount;
+}
+
+int TileMap::readFromData(const Tbyte* data,
+			   TileMapFormat format__,
+			   int w__,
+			   int h__,
+			   Tbyte upperByte__) {
+  int byteCount = 0;
+  
+  // Set format data
+  format_ = format__;
+  w_ = w__;
+  h_ = h__;
+  
+  // Reinitialize tile array
+  reinitializeTileData(w_, h_);
+  
+  // Read tile identifiers
+  for (int j = 0; j < h_; j++) {
+    for (int i = 0; i < w_; i++) {
+	  // Read low byte of identifier
+	  unsigned int identifier = ByteConversion::fromBytes(
+	                              data + byteCount,
+								  ByteSizes::uint8Size,
+								  EndiannessTypes::little,
+								  SignednessTypes::nosign);
+      
+      // Incorporate upper byte
+      identifier |= (((unsigned int)(upperByte__)) << ByteSizes::uint8Size);
+	  
+	  // Place in array
+	  tileData_[i][j] = TileReference(identifier);
+      
+	  byteCount += ByteSizes::uint8Size;
+	}
+  }
   
   return byteCount;
 }
@@ -29,7 +126,72 @@ void TileMap::save(std::string& data) const {
 int TileMap::load(const Tbyte* data) {
   int byteCount = 0;
   
+  
+  
   return byteCount;
+}
+
+TileMap::TileMapFormat TileMap::format() const {
+  return format_;
+}
+
+int TileMap::w() const {
+  return w_;
+}
+
+int TileMap::h() const {
+  return h_;
+}
+
+int TileMap::lowerLimit() const {
+  return lowerLimit_;
+}
+
+int TileMap::upperLimit() const {
+  return upperLimit_;
+}
+
+void TileMap::setFormat(TileMapFormat format__) {
+  format_ = format__;
+}
+
+void TileMap::setW(int w__) {
+  w_ = w__;
+}
+
+void TileMap::setH(int h__) {
+  h_ = h__;
+}
+
+void TileMap::setLowerLimit(int lowerLimit__) {
+  lowerLimit_ = lowerLimit__;
+}
+
+void TileMap::setUpperLimit(int upperLimit__) {
+  upperLimit_ = upperLimit__;
+}
+
+void TileMap::destroyTileData() {
+  // Delete column arrays
+  for (int i = 0; i < w_; i++) {
+    delete tileData_[i];
+  }
+  
+  // Delete container array
+  delete tileData_;
+}
+
+void TileMap::reinitializeTileData(int w__, int h__) {
+  // Destroy existing data
+  destroyTileData();
+  
+  // Initialize column array
+  tileData_ = new TileReference*[w_];
+  
+  // Initialize columns
+  for (int i = 0; i < w_; i++) {
+    tileData_[i] = new TileReference[h_];
+  }
 }
 
 
