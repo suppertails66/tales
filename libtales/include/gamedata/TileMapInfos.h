33ae7d370b525c10b4bd3762af4ba21fc2ee4d83
Supper <suppertails66@gmail.com>
2016-01-22 19:35:47 -0600

    Add TileMapInfos class

---
+++ libtales/include/gamedata/TileMapInfos.h
@@ -0,0 +1,39 @@
+#ifndef TILEMAPINFOS_H
+#define TILEMAPINFOS_H
+
+
+#include "gamedata/TileMapInfo.h"
+
+namespace Tales {
+
+
+class TileMapInfos {
+public:
+  /**
+   * Returns number of tilemaps.
+   * @return Number of tilemaps.
+   */
+  static int numTileMaps();
+  
+  /**
+   * Returns TileMapInfo for the given tilemap index.
+   * @return TileMapInfo for the given tilemap index.
+   */
+  static TileMapInfo tileMapInfo(int index);
+protected:
+  /**
+   * Number of tilemaps.
+   */
+  const static int numTileMaps_ = 183;
+  
+  /**
+   * Array of tilemap infos.
+   */
+  const static TileMapInfo tileMapInfos_[];
+};
+
+
+};
+
+
+#endif
